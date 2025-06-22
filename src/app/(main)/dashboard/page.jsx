73af06d37aa4c3d2ab5afabd5e8e783da90d89a8
dhruvikmodi23 <dhruvikmodi23@gmail.com>
2025-06-22 17:53:05 +0530

    Add interview-prep module

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -1,9 +1,12 @@
 
+import { getIndustryInsights } from "@/actions/dashboard";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
+import DashboardView from "./_components/dashboard-view";
 
 export default async function DashboardPage() {
   const { isOnboarded } = await getUserOnboardingStatus();
+  const insights = await getIndustryInsights();
 
   // If not onboarded, redirect to onboarding page
   // Skip this check if already on the onboarding page
@@ -13,8 +16,8 @@ export default async function DashboardPage() {
 
 
   return (
-    <div>
-
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
     </div>
   );
 }
